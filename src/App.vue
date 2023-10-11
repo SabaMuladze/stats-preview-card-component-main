f38bb47b9089fe24c0446c5609487acf3f180144
SabaMuladze <muladze66@bk.ru>
2023-10-12 01:06:39 +0400

    Done

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,38 @@
 
 <template>
-    <div class="min-h-screen w-full bg-[#0A0C1C] flex flex-col max-md:py-20 max-md:px-6">
-        <div class="max-md:w-full bg-[#1B1937] rounded-lg">
-            <div class="z-20 opacity-75 image relative">
-                <img class="rounded-lg" src="src/assets/image-header-mobile.jpg" alt="">
+    <div class="min-h-screen w-full bg-[#0A0C1C] flex flex-col py-20 px-6 md:items-center md:justify-center ">
+        <div class="max-md:w-full bg-[#1B1937] rounded-lg md:flex md:flex-row-reverse max-w-[1100px]">
+            <div class="z-20 opacity-75 relative image md:w-[50%]">
+                <img class="rounded-lg  md:hidden object-cover w-full" src="src/assets/image-header-mobile.jpg" alt="">
+                <div class="max-sm:hidden img rounded-lg"></div>
             </div>
-            <div class="px-6 mt-6 flex flex-col gap-6 text-center ">
-                <h1 class="text-2xl">Get <span class="text-[#AB5CDB]">insights</span> that help your business
-                    grow.
-                </h1>
-                <p class="opacity-75">Discover the benefits of data analytics and make better decisions regarding revenue,
-                    customer
-                    experience,
-                    and overall efficiency.</p>
-            </div>
-            <div class="flex flex-col items-center mt-10 mb-8 gap-6">
-                <div class="text-center">
-                    <h2 class="text-2xl font-bold">{{ companies }}</h2>
-                    <p class=" text-sm opacity-75">COMPANIES</p>
+            <div class="md:flex-1 md:pt-16 md:px-14 mt-10">
+                <div class="px-6 mt-6 flex flex-col gap-6 text-center md:text-left md:px-0 md:mt-0">
+                    <h1 class="text-2xl md:text-4xl">Get <span class="text-[#AB5CDB]">insights</span> that help your
+                        business
+                        grow.
+                    </h1>
+                    <p class="opacity-75">Discover the benefits of data analytics and make better decisions regarding
+                        revenue,
+                        customer
+                        experience,
+                        and overall efficiency.</p>
                 </div>
-                <div class="text-center">
-                    <h2 class="text-2xl font-bold">{{ templates }}</h2>
-                    <p class=" text-sm opacity-75">TEMPLATES</p>
+                <div
+                    class="flex flex-col items-center mt-10 mb-8 gap-6 md:flex-row md:justify-between lg:pb-8 md:mt-[72px]">
+                    <div class="text-center md:text-left">
+                        <h2 class="text-2xl font-bold">{{ companies }}</h2>
+                        <p class=" text-sm opacity-75">COMPANIES</p>
+                    </div>
+                    <div class="text-center md:text-left">
+                        <h2 class="text-2xl font-bold">{{ templates }}</h2>
+                        <p class=" text-sm opacity-75">TEMPLATES</p>
+                    </div>
+                    <div class="text-center md:text-left">
+                        <h2 class="text-2xl font-bold">{{ queries }}</h2>
+                        <p class=" text-sm opacity-75">QUERIES</p>
+                    </div>
                 </div>
-                <div class="text-center">
-
-                    <h2 class="text-2xl font-bold">{{ queries }}</h2>
-                    <p class=" text-sm opacity-75">QUERIES</p>
-                </div>
-
-
             </div>
         </div>
     </div>
@@ -60,7 +63,7 @@ p {
     content: '';
     height: 100%;
     opacity: 70%;
-    border-radius: 8px;
+    border-radius: 5px;
     mix-blend-mode: multiply;
     width: 100%;
     background-color: #AB5CDB;
@@ -69,5 +72,14 @@ p {
     left: 0;
     top: 0;
     z-index: 100;
+}
+
+.img {
+    width: 100%;
+    height: 100%;
+    background-image: url('src/assets/image-header-desktop.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    object-fit: cover;
 }
 </style>
